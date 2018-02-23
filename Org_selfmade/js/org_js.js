832851811bb9c16661c9dd63e6619aced35b1493
Michaelpp <cloudburn.y@gmail.com>
2018-02-23 11:36:11 +0800

    update

---
+++ Org_selfmade/js/org_js.js
@@ -0,0 +1,42 @@
+
+/*script slide images*/
+
+//auto slides
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides"); 
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+var mslideIndex = 1;
+mshowSlides(mslideIndex);
+
+//maunal slides
+function plusSlides(n) {
+  mshowSlides(mslideIndex += n);
+}
+
+function currentSlide(n) {
+  mshowSlides(mslideIndex = n);
+}
+
+function mshowSlides(n) {
+  var i;
+  var mslides = document.getElementsByClassName("mySlides");
+  if (n > mslides.length) {mslideIndex = 1}    
+  if (n < 1) {mslideIndex = mslides.length}
+  for (i = 0; i < mslides.length; i++) {
+      mslides[i].style.display = "none";  
+  }
+  mslides[mslideIndex-1].style.display = "block";  
+}
+
