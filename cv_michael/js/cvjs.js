f47e4938591924fef7b7db75373bdf03d30b264d
Michaelpp <cloudburn.y@gmail.com>
2018-03-13 10:33:03 +0800

    Update cvjs.js

--- cv_michael/js/cvjs.js
+++ cv_michael/js/cvjs.js
@@ -10,18 +10,6 @@ $(document).ready(function(){
   });
 });
 
-/*
-$(document).ready(function(){
-    $(".hideit").fadeIn(5000).removeClass("hideit");
-    $('a[href*="#"]').on("click", function(event){
-      event.preventDefault();
-    $("html, body").animate({
-        scrollTop: $($(this).attr("href")).offset().top
-      }, 700, 'linear');
-  });
-});
-*/
-
 
 $(document).ready(function(){
  	$(window).scroll(function(){
